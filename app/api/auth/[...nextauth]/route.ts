53b5ca592fc7e3afcb91f471ab93bbf16d107887
superiorkid <bayiPetani@protonmail.com>
2023-10-01 22:27:25 +0800

    complete :: user credentials login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,10 +4,62 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { loginSchama } from "@/lib/validations/auth.validation";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "Enter email address",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "******",
+        },
+      },
+      async authorize(credentials, req) {
+        // validation
+        const validation = loginSchama.safeParse({
+          email: credentials?.email,
+          password: credentials?.password,
+        });
+
+        if (!validation.success) {
+          return null;
+        }
+
+        // check account already in database
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        // check password is match
+        const passwordMatches = await bcrypt.compare(
+          credentials?.email as string,
+          user.password as string
+        );
+
+        if (!passwordMatches) {
+          return null;
+        }
+
+        // return user
+        return user;
+      },
+    }),
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,26 +1,66 @@
+"use client";
+
+import { useTransition } from "react";
+import { TLogin, loginSchama } from "@/lib/validations/auth.validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 const LoginForm = () => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<TLogin>({
+    resolver: zodResolver(loginSchama),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onLoginSubmit = (values: TLogin) => {
+    startTransition(() => {
+      signIn("credentials", { ...values, redirect: false })
+        .then((response) => {
+          toast.success("Login Successfully");
+          router.push("/");
+        })
+        .catch((error) => {
+          toast.error("Login failed");
+        });
+    });
+  };
+
   return (
-    <form className="space-y-2">
+    <form className="space-y-2" onSubmit={handleSubmit(onLoginSubmit)}>
       <input
         type="text"
-        name="email"
-        id="email"
         placeholder="Email"
-        className="auth-input"
+        className={cn("auth-input", isPending && "text-gray-300")}
+        {...register("email")}
       />
       <input
         type="password"
-        name="password"
-        id="password"
         placeholder="Password"
-        className="auth-input"
+        className={cn("auth-input", isPending && "text-gray-300")}
+        {...register("password")}
       />
 
       <button
         type="submit"
-        className="border bg-sky-500 text-white font-semibold w-full rounded-lg py-1.5 text-sm"
+        className={cn(
+          "border bg-sky-500 text-white font-semibold w-full rounded-lg py-1.5 text-sm",
+          isPending && "bg-gray-300"
+        )}
       >
-        Log in
+        {isPending ? "..." : "Log in"}
       </button>
     </form>
   );
