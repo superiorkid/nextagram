b877392b9518c56d0b760f67b7a971a59a40f4d7
superiorkid <bayiPetani@protonmail.com>
2023-10-02 08:48:12 +0800

    fix sidebar logo

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -15,41 +15,41 @@ const MainLayuot = ({ children }: Props) => {
     <div className="flex min-h-screen space-x-7">
       <aside className="w-[18dvw] flex flex-col justify-between border-r border-gray-300 shadow-sm pt-12 pb-6 px-4">
         <div>
-          <Logo className="h-[32px] flex items-start mb-8" />
-          <nav className="flex flex-col space-y-2.5">
+          <Logo className="h-[38px] flex items-start mb-8 ml-3 w-[120px]" />
+          <nav className="flex flex-col space-y-3">
             <Link
               href="/"
               className="text-base font-bold hover:bg-gray-100 py-2.5 px-3 rounded-md"
             >
-              <GoHomeFill className="w-7 h-7 inline mr-2.5" />
+              <GoHomeFill className="w-6 h-6 inline mr-2.5" />
               Home
             </Link>
             <Link
               href="#search"
               className="text-base font-light hover:bg-gray-100 py-2.5 px-3 rounded-md"
             >
-              <GoSearch className="w-7 h-7 inline mr-2.5" />
+              <GoSearch className="w-6 h-6 inline mr-2.5" />
               Search
             </Link>
             <Link
               href="#explore"
               className="text-base font-light hover:bg-gray-100 py-2.5 px-3 rounded-md"
             >
-              <FiCompass className="w-7 h-7 inline mr-2.5" />
+              <FiCompass className="w-6 h-6 inline mr-2.5" />
               Explore
             </Link>
             <Link
               href="#create-pop-up"
               className="text-base font-light hover:bg-gray-100 py-2.5 px-3 rounded-md"
             >
-              <GoPlusCircle className="w-7 h-7 inline mr-2.5" />
+              <GoPlusCircle className="w-6 h-6 inline mr-2.5" />
               Create
             </Link>
             <Link
               href="#profile"
               className="text-base font-light hover:bg-gray-100 py-2.5 px-3 rounded-md"
             >
-              <RxAvatar className="w-7 h-7 inline mr-2.5" />
+              <RxAvatar className="w-6 h-6 inline mr-2.5" />
               Profile
             </Link>
           </nav>
