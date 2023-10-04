dc2138f097e15c87f5bd067729f3b6a805db3706
superiorkid <bayiPetani@protonmail.com>
2023-10-04 13:30:18 +0800

    loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Logo from "@/components/logo";
+import Image from "next/image";
+import nextagramLogo from "@/public/assets/nextagram_logo.svg";
 
 function Loading() {
   return (
     <div className="flex justify-center min-h-screen items-center">
-      <Logo className="h-24" />
+      <div className="relative w-44 h-44 lg:w-[11dvw] lg:h-[22dvh] animation-pulse">
+        <Image
+          fill
+          priority
+          src={nextagramLogo}
+          alt="nextagram logo"
+          className="object-contain"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
     </div>
   );
 }
